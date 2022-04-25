2698e1878f6ac20dc94e68d180d444382bb63da5
Nick Cao <nickcao@nichi.co>
2022-04-25 22:26:11 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+use shadowsocks_service::shadowsocks::relay::socks5::{
+    self, Address, Command, HandshakeRequest, HandshakeResponse, TcpRequestHeader,
+    TcpResponseHeader,
+};
+use nix::sys::socket::{setsockopt, sockopt::BindToDevice};
+use std::ffi::OsString;
+use std::net::{Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
+use std::os::unix::prelude::AsRawFd;
+use tokio::net::{TcpListener, TcpSocket, TcpStream};
+use argh::FromArgs;
+
+#[derive(FromArgs)]
+/// grasp
+struct Args {
+    /// listen address
+    #[argh(option, short = 'l')]
+    listen: SocketAddr,
+    /// bind address
+    #[argh(option, short = 'b')]
+    bind: Ipv6Addr,
+    /// bind interface
+    #[argh(option, short = 'i')]
+    interface: OsString,
+    /// nat64 prefix
+    #[argh(option, short = 'p')]
+    prefix: Ipv6Addr
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Args = argh::from_env();
+    let listener = TcpListener::bind(args.listen).await?;
+    while let Ok((incoming, _)) = listener.accept().await {
+        tokio::spawn(process(incoming, args.bind, args.interface.clone(), args.prefix));
+    }
+    Ok(())
+}
+
+fn dns64(addr: SocketAddrV4, prefix: Ipv6Addr) -> SocketAddrV6 {
+    let seg4 = addr.ip().to_ipv6_mapped().segments();
+    let seg6 = prefix.segments();
+    SocketAddrV6::new(
+        Ipv6Addr::new(
+            seg6[0],
+            seg6[1],
+            seg6[2],
+            seg6[3],
+            seg6[4],
+            seg6[5],
+            seg4[6],
+            seg4[7],
+        ),
+        addr.port(),
+        0,
+        0,
+    )
+}
+
+async fn process(mut inbound: TcpStream, bind: Ipv6Addr, interface: OsString, prefix: Ipv6Addr) -> Result<(), std::io::Error> {
+    HandshakeRequest::read_from(&mut inbound).await?;
+    HandshakeResponse::new(socks5::SOCKS5_AUTH_METHOD_NONE)
+        .write_to(&mut inbound)
+        .await?;
+    let header = TcpRequestHeader::read_from(&mut inbound).await?;
+    match header.command {
+        Command::TcpConnect => {
+            let addr = match header.address {
+                Address::SocketAddress(SocketAddr::V6(addr)) => addr,
+                Address::SocketAddress(SocketAddr::V4(addr)) => dns64(addr, prefix),
+                Address::DomainNameAddress(domain, port) => {
+                    tokio::net::lookup_host((domain.as_str(), port))
+                        .await?
+                        .find_map(|addr| match addr {
+                            SocketAddr::V4(a) => Some(dns64(a, prefix)),
+                            SocketAddr::V6(a) => Some(a),
+                        })
+                        .unwrap()
+                }
+            };
+            let outbound = TcpSocket::new_v6()?;
+            setsockopt(outbound.as_raw_fd(), BindToDevice, &interface)?;
+            outbound.bind(SocketAddr::V6(SocketAddrV6::new(bind, 0, 0, 0)))?;
+            let mut conn = outbound.connect(SocketAddr::from(addr)).await?;
+            TcpResponseHeader::new(
+                socks5::Reply::Succeeded,
+                Address::SocketAddress(SocketAddr::from(addr)),
+            )
+            .write_to(&mut inbound)
+            .await?;
+            tokio::io::copy_bidirectional(&mut inbound, &mut conn).await?;
+            Ok(())
+        }
+        _ => unimplemented!(),
+    }
+}
